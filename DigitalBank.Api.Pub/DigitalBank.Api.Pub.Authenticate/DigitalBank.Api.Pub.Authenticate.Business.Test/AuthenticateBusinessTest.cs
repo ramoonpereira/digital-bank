cfbf33f0e44118ca4e568f2259d09951a516b74a
Ramon Pereira <ramon.pereira@mag-it.com.br>
2019-10-24 09:12:39 -0300

    setup projects testes

--- DigitalBank.Api.Pub/DigitalBank.Api.Pub.Authenticate/DigitalBank.Api.Pub.Authenticate.Business.Test/AuthenticateBusinessTest.cs
+++ DigitalBank.Api.Pub/DigitalBank.Api.Pub.Authenticate/DigitalBank.Api.Pub.Authenticate.Business.Test/AuthenticateBusinessTest.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DigitalBank.Api.Pub.Authenticate.Business.Implementations;
+using DigitalBank.Api.Pub.Authenticate.Business.Interfaces;
+using DigitalBank.Api.Pub.Authenticate.Security.Encryptor.Handler.Interfaces;
+using DigitalBank.Api.Pub.Authenticate.Security.JWT.Handler.Interfaces;
+using Moq;
+using NUnit.Framework;
 
 namespace DigitalBank.Api.Pub.Authenticate.Business.Test
 {
     public class AuthenticateBusinessTest
     {
+        private AuthenticateBusiness _authenticateBusiness;
+        private Mock<ICustomerBusiness> _customerBusinessMock;
+        private Mock<IPermissionBusiness> _permissionBusinessMock;
+        private Mock<IEncryptorHandler> _encryptorHandlerMock;
+        private Mock<ITokenHandler> _tokenHandlerMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _customerBusinessMock = new Mock<ICustomerBusiness>();
+            _permissionBusinessMock = new Mock<IPermissionBusiness>();
+            _encryptorHandlerMock = new Mock<IEncryptorHandler>();
+            _tokenHandlerMock = new Mock<ITokenHandler>();
+
+            _authenticateBusiness = new AuthenticateBusiness(
+                _customerBusinessMock.Object,
+                _encryptorHandlerMock.Object,
+                _tokenHandlerMock.Object,
+                _permissionBusinessMock.Object);
+        }
     }
 }
--- DigitalBank.Api.Pub/DigitalBank.Api.Pub.Authenticate/DigitalBank.Api.Pub.Authenticate.Test/v1/AuthenticateControllerTest.cs
+++ DigitalBank.Api.Pub/DigitalBank.Api.Pub.Authenticate/DigitalBank.Api.Pub.Authenticate.Test/v1/AuthenticateControllerTest.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using DigitalBank.Api.Pub.Authenticate.Business.Interfaces;
+using DigitalBank.Api.Pub.Authenticate.Controllers.v1;
+using Moq;
+using NUnit.Framework;
 
 namespace DigitalBank.Api.Pub.Authenticate.Test.v1
 {
     public class AuthenticateControllerTest
     {
+        private AuthenticateController _controller;
+        private Mock<IAuthenticateBusiness> _authenticateBusinessMock;
+        private Mock<IMapper> _mapperMock;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _authenticateBusinessMock = new Mock<IAuthenticateBusiness>();
+            _mapperMock = new Mock<IMapper>();
+
+            _controller = new AuthenticateController(_authenticateBusinessMock.Object, _mapperMock.Object);
+        }
     }
 }
